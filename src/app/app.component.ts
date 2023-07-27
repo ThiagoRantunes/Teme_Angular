8b6ede9d04e8b018d1c1be7dcf44231925e945d9
unknown <thiagorantunes05@gmail.com>
2023-07-27 18:31:29 -0300

    Teme_Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Teme_Angular';
+
+  class = ["Twhite","Tblack"]
+
+  btnName = ["White","Black"];
+
+  i: number = 0;
+
+  teme(){
+    if(this.i == 0){
+      this.i++;
+    }else if(this.i == 1){
+      this.i--;
+    }
+    
+  }
+
 }
